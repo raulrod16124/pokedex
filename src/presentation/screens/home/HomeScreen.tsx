ebaa95e28d5582a001b7033bc6497fbeaba94742
raul rodriguez <raul.rod16124@gmail.com>
2024-09-10 22:20:32 +0100

    install react native paper

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native-paper'
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   )
 }
