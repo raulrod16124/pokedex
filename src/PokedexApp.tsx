d45a50e24fe6165a17c8782b8d5afd98f57760db
raul rodriguez <raul.rod16124@gmail.com>
2024-09-11 12:54:46 +0100

    define interface and api request

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,14 +1,17 @@
 import 'react-native-gesture-handler';
 
-import { NavigationContainer } from '@react-navigation/native'
 import { StackNavigator } from './presentation/navigator/StackNavigator';
-import { PaperProvider } from 'react-native-paper';
 import { ThemeContextProvider } from './presentation/context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 export const PokedexApp = () => {
   return (
-    <ThemeContextProvider>
-      <StackNavigator />
-    </ThemeContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContextProvider>
+        <StackNavigator />
+      </ThemeContextProvider>
+    </QueryClientProvider>
   )
 }
