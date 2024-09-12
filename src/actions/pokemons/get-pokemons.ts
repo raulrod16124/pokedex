bf2074f3786ba3e5c479ab5f3b216f1c49004afc
raul rodriguez <raul.rod16124@gmail.com>
2024-09-12 12:23:31 +0100

    fix error on get number of pokemons

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -5,7 +5,7 @@ import { PokemonMapper } from "../../infrastructure/mappers/pokemon.mapper";
 
 export const getPokemons = async (page: number, limit: number = 20): Promise<Pokemon[]> => {
     try{
-        const url = `/pokemon?offset=${page * 10}&limit=${limit}`;
+        const url = `/pokemon?offset=${page * 20}&limit=${limit}`;
         const {data} = await pokeApi.get<PokeAPIPaginatedResponse>(url);
 
         const pokemonPromises = data.results.map((info) => {
